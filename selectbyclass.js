21a948a6a2867dfaafadf25ab40305bb297ef6ab
Nuclear One <ajithsnair10@gmail.com>
2022-02-15 23:15:12 +0530

    select by js DOM

---
+++ selectbyclass.js
@@ -0,0 +1,22 @@
+//console.dir(document);
+//console.log(document.title)
+//document.title='pro js coder';
+//console.log(document.title)
+//console.log(document.all[15])
+//document.all[15].textContent='Your Hacked';
+
+console.log(document.getElementsByClassName('container'))
+let text=document.getElementsByClassName('head1');
+console.log(text);
+
+var FF=document.getElementsByClassName('head1');
+console.log(FF);
+ let ite=document.getElementsByClassName('item')
+ console.log(ite[2])
+
+ ite[2].textContent='Hello 2'
+ite[2].style.background='yellow';
+for(var i=0;i<ite.length;i++){
+    ite[i].style.fontweight='Bold';
+    ite[i].style.color='brown'
+}
