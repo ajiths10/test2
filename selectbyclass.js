c2721b3489481b0b8681315939ebc8c37ddbec61
Nuclear One <ajithsnair10@gmail.com>
2022-02-17 10:10:30 +0530

    add upated

--- selectbyclass.js
+++ selectbyclass.js
@@ -5,18 +5,31 @@
 //console.log(document.all[15])
 //document.all[15].textContent='Your Hacked';
 
-console.log(document.getElementsByClassName('container'))
-let text=document.getElementsByClassName('head1');
+//console.log(document.getElementsByClassName('container'))
+//let text=document.getElementsByClassName('head1');
+//console.log(text);
+
+//var FF=document.getElementsByClassName('head1');
+//console.log(FF);
+// let ite=document.getElementsByClassName('item')
+// console.log(ite[2])
+
+ //ite[2].textContent='Hello 2'
+//ite[2].style.background='yellow';
+//for(var i=0;i<ite.length;i++){
+//    ite[i].style.fontweight='Bold';
+//    ite[i].style.color='brown'
+//}
+
+console.log(document.getElementsByTagName('li'))
+let text=document.getElementsByTagName('form');
 console.log(text);
 
-var FF=document.getElementsByClassName('head1');
+var FF=document.getElementsByClassName('li');
 console.log(FF);
  let ite=document.getElementsByClassName('item')
  console.log(ite[2])
 
- ite[2].textContent='Hello 2'
-ite[2].style.background='yellow';
-for(var i=0;i<ite.length;i++){
-    ite[i].style.fontweight='Bold';
-    ite[i].style.color='brown'
-}
+ for(var i=0;i<ite.length;i++){
+    ite[i].style.color='red'
+ }
